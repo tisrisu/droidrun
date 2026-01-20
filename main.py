747bc6c1f3a9958772aa40120d698572332ec58c
tisrisu <tisrisu123@gmail.com>
2026-01-20 16:21:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+from droidrun import DroidAgent, DroidrunConfig, AgentConfig, AdbTools
+from llama_index.llms.google_genai import GoogleGenAI
+import os
+
+async def main():
+
+    llm = GoogleGenAI(api_key = os.environ.get("GOOGLE_API_KEY"), model="models/gemini-2.5-flash")
+    # Initialize the DroidAgent
+    droid_agent = DroidAgent(
+"Sync Tuesday's class schedule from WhatsApp to Alarms.Step 1: Open WhatsApp, search for the contact 'teacher', and open the chat.Step 2: Find the message or file named 'tuesday_only_timetable'. Open it and take a screenshot to extract the class times for Tuesday.Step 3: Close WhatsApp and open the Gemini App.Step 4: For each class time found in Step 2, type a command to Gemini: 'Set an alarm for [Time]'.Step 5: Confirm the alarm is set before moving to the next one." ,     
+     config=DroidrunConfig(
+            agent=AgentConfig(max_steps=100)),
+            llms = llm,
+            tools=AdbTools()
+        )
+    
+    await droid_agent.run()
+
+    
+if __name__ == "__main__":
+    asyncio.run(main())
